13e5a360a33032ecfefd3924620167163529584f
wasifalam23 <wsfalam84@gmail.com>
2022-12-24 20:37:07 +0530

    removed moragan

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const cors = require('cors');
-const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 const blogRouter = require('./routes/blogRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -16,10 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'uploads')));
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 app.use('/api/v1/blogs', blogRouter);
 app.use('/api/v1/users', userRouter);
