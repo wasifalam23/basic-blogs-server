4cd550929c5e750e378603fb9d03144bdf0b8e98
wasifalam23 <wsfalam84@gmail.com>
2022-12-25 10:26:18 +0530

    added cors origin to *

--- app.js
+++ app.js
@@ -10,18 +10,7 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-// app.use(cors({ origin: true, credentials: true }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-  next();
-});
+app.use(cors({ origin: '*', credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
