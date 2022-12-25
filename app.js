99130f7082cafd9e08e57e383f6eeccafe93752a
wasifalam23 <wsfalam84@gmail.com>
2022-12-25 10:23:09 +0530

    added res headers cors and changed to previous settings on authController

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-app.use(cors({ credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
